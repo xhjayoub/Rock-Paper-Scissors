ef82d72d9eeae4300c1eaff8ee872bc7475211de
Ayoub hj <medayoubhajamor197@gmail.com>
2025-03-14 01:58:17 +0100

    Add getPlayerInput()

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+void getPlayerInput(int playerN, char *p)
+{
+	printf("Player %d : ", playerN);
+	scanf("%8s", p);
+}
 
 int main()
 {
 	printf("Rock Paper Scissors!\n");
 
+	char p1[8];
+	char p2[8];
+
+	getPlayerInput(1, p1);
+	getPlayerInput(2, p2);
 	return 0;
 }
